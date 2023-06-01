e7d121f0981f9707f9de579cd44029011f28667e
AlvaroAGDuoc <a.aguerog@duocuc.cl>
2023-06-01 19:59:37 -0400

    nuevo mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'mobileAlces',
+  appName: 'alcesMobile',
   webDir: 'www',
   server: {
     androidScheme: 'https'
