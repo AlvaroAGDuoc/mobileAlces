558bc1bbcfd24d27ab484abcbce0056d672fdd51
GatoMamado <lu.morgado@duocuc.cl>
2023-06-12 18:42:13 -0400

    Vista login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'inicio', // Establece el nombre de la ruta de la página que deseas como página raíz
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: '',
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
