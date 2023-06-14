b5d200798277afef274f8d6332325fec48a3be2c
GatoMamado <lu.morgado@duocuc.cl>
2023-06-13 22:37:11 -0400

    Diseño de tabla liga y color de las tabs

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
+ iniciarSesion(){
+  this.route.navigate(['/tabs/ligas']);
+ }
 }
